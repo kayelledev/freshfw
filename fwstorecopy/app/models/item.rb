a1821a096566009a981460603ad238fc58ff0678
agient <agilityent@gmail.com>
2015-04-26 23:41:20 -0400

    modified shoppe

--- fwstorecopy/app/models/item.rb
+++
@@ -1,8 +0,0 @@
-class Item  < ActiveRecord::Base
-  belongs_to :product, :class_name => 'Shoppe::Product'
-  validates :item_name, :presence => true
-  validates :item_sku, :presence => true
-  validates :item_type, :presence => true
-  validates :item_description, :presence => true
-
-end
--- fwstorecopy/db/migrate/20150425062625_add_country_province_to_tax_rates.rb
+++
@@ -1,6 +0,0 @@
-class AddCountryProvinceToTaxRates < ActiveRecord::Migration
-  def change
-    add_column :shoppe_tax_rates, :country, :string
-    add_column :shoppe_tax_rates, :province, :string
-  end
-end
