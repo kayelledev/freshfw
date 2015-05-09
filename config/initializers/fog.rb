c670090e27c6779954daf6f22bc9319746967400
agient <agilityent@gmail.com>
2015-05-09 14:40:09 -0400

    removed key

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,13 +1,15 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAINQA3XAMH7BDRHKQ',                        # required
-    aws_secret_access_key: '9KdoryimJ4w8loPH/VAEWKjCCbRDAWbsQxiuk7La'                       # required
-#    host:                  's3.example.com',             # optional, defaults to nil
- #   endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = 'uploads'                          # required
-#  config.fog_public     = false                                        # optional, defaults to true
-# config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-end
+if Rails.env.production? 
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['S3_Access_Key'],                       # required
+      aws_secret_access_key: ENV['S3_Secret_Key']                      # required
+  #    host:                  's3.example.com',             # optional, defaults to nil
+   #   endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.fog_directory  = ENV['S3_Bucket']                          # required
+  #  config.fog_public     = false                                        # optional, defaults to true
+  # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
+end 
