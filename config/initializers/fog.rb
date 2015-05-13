8c17bd00f2a42f88b431eaaffb963d957b21f482
bernstein <clapandslap@gmail.com>
2015-05-13 12:50:51 +0300

    fog production config temporary muted till we have no credentials

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,4 +1,4 @@
-if Rails.env.production? 
+if false #Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
