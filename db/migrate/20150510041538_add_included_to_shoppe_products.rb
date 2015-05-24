e65c9e07c09efa384f8a9da373687bcace6a7141
agient <agilityent@gmail.com>
2015-05-24 17:45:49 -0400

    add postgres version of migration for adding included

--- db/migrate/20150510041538_add_included_to_shoppe_products.rb
+++ db/migrate/20150510041538_add_included_to_shoppe_products.rb
@@ -1,5 +1,5 @@
 class AddIncludedToShoppeProducts < ActiveRecord::Migration
   def change
-    add_column :shoppe_products, :included, :array
+    add_column :shoppe_products, :included, :integer, array: true, default: '{}'
   end
 end
