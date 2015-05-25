80ba593f81cbadbc1a315ec4c00d7d1fb7b1f375
bernstein <clapandslap@gmail.com>
2015-05-25 16:13:06 +0300

    AddIncludedToShoppeProducts array migration muted

--- db/migrate/20150510041538_add_included_to_shoppe_products.rb
+++ db/migrate/20150510041538_add_included_to_shoppe_products.rb
@@ -1,5 +1,5 @@
-class AddIncludedToShoppeProducts < ActiveRecord::Migration
-  def change
-    add_column :shoppe_products, :included, :array
-  end
-end
+# class AddIncludedToShoppeProducts < ActiveRecord::Migration
+#   def change
+#     add_column :shoppe_products, :included, :array
+#   end
+# end
