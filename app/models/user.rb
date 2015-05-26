e322a70c742a88eec7a6f51e4a8a0269346bf7f4
bernstein <clapandslap@gmail.com>
2015-05-26 18:38:55 +0300

    user welcome msg and logout btn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :confirmable
+         :recoverable, :rememberable, :confirmable,
+         :omniauthable
+
+
+  def fullname
+   first_name + ' ' + last_name
+  end
 end
