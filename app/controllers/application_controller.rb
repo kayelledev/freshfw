3d2bc8be28a35ecb8ee2ac7739841880f0c42c3d
bernstein <clapandslap@gmail.com>
2015-05-27 14:41:53 +0300

    add firstname and lastname to sing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
 
@@ -69,4 +70,8 @@ class ApplicationController < ActionController::Base
       params[:order_tax_rate] = @db_tax_rate
       
     end
+
+  def configure_permitted_parameters
+    (devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]).flatten!
+  end
 end
