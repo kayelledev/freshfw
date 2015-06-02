71e61cf902dc39d2dfd48f69a5ccec44bff12bad
bernstein <clapandslap@gmail.com>
2015-06-02 11:50:27 +0300

    production env smtp configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,22 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.zoho.com",
+    :port      => 465, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "info@gofourwalls.com",
+    :password  => "fourwalls7165165", # SMTP password is any valid API key
+    :authentication => 'plain', # Mandrill supports 'plain' or 'login'
+    :domain => 'fresh-fw.herokuapp.com', # your domain to identify your server when connecting
+    :ssl => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_options = {from: "GoFourWalls <info@gofourwalls.com>"}
+  config.action_mailer.default_url_options = {:host => 'fresh-fw.herokuapp.com'}
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
