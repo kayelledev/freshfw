b03bac8a062bdfca6c4b320f3d8def7fcb91d97e
Dima <gunosada1@gmail.com>
2015-09-28 14:26:39 +0300

    add changes on product page

--- app/assets/javascripts/products/products.js
+++ app/assets/javascripts/products/products.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('input[name=varsku]').on('change', function(e) {
     var product = $('#product_' + e.target.value);
+    var descr_prod = $('#description_product_' + e.target.value);
     $('.variant').hide();
     $(product).show();
+    $(descr_prod).show();
     $('#buy').attr('href', product.data('url'));
   });
 });
