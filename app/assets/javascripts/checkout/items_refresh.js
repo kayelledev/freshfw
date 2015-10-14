a500150c176f9823af65a1289c70b92dae1c709c
v7m <deathside7@gmail.com>
2015-10-14 22:36:56 +0300

    Precompile assets

--- app/assets/javascripts/checkout/items_refresh.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('input[name="order[delivery_service_id]"]').change(function(e){
-    var url = $(e.target).parent().data('refreshPath');
-    var value = $(e.target).val();
-
-    $.post( url, { delivery_service_id: value});
-  });
-});
