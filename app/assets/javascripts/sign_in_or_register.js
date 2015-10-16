9c245b31fd5b9dd78e6b8179bed1fe4b1089abc9
v7m <deathside7@gmail.com>
2015-10-16 14:58:28 +0300

    Fix sign up js bug

--- app/assets/javascripts/sign_in_or_register.js
+++ app/assets/javascripts/sign_in_or_register.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    var changeForm = $('#change-form');
-    var changeFormText = $('#change-form p');
+    var changeForm = $('.forms-links #change-form');
+    var changeFormText = $('.forms-links #change-form p');
     var columnTitle = $('.sign-in-or-register .title .form-title p')
     var signInForm = $('.sign-in-or-register .sign-in-or-register-forms .sign-in');
     var createAccountForm = $('.sign-in-or-register .sign-in-or-register-forms .create-account');
-    $('body').on('click', changeForm, function(e){
+    changeForm.on('click', function(e){
         e.preventDefault();
         if (signInForm.css('display') == 'block' && createAccountForm.css('display') == 'none') {
             signInForm.fadeOut(400, function(){
