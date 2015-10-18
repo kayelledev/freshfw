c6d2d30f47d1b1d56c1993625072ff0b3a28e7c6
v7m <deathside7@gmail.com>
2015-10-18 21:55:54 +0300

    Fix cart table, create account, blog

--- app/assets/javascripts/item_quantity.js
+++ app/assets/javascripts/item_quantity.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+    // change  quantity
     $(document).on('change', '#quantity-select', function(e) {
         var orderId = $('.shopping-cart').data("order");
-        var itemId = $(this).parent().parent().data("item");
+        var itemId = $(this).parent().parent().parent().data("item");
         var itemQuntity = $("#quantity-select option:selected").text();
-        var url = $(e.target).parent().data('refreshPath');
+        var url = $(e.target).parent().parent().data('refreshPath');
 
         $.post( url, {
             item_id: itemId,
