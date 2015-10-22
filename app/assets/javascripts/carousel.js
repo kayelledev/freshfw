a3c025e4dd81c63160db332f11c588e329adb089
v7m <deathside7@gmail.com>
2015-10-22 17:43:06 +0300

    Fix carousel resizing on main page

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -2,24 +2,25 @@ $(document).ready(function() {
     var windowWidth = $(window).width();
     //resize carousel
     function resizeCarousel (windowWidth) {
-        if (windowWidth > 800) {
+        if (windowWidth > 1200) {
             var windowHeight = $(window).height();
             var headerHeight = $('#header').height();
             $('#myCarousel').css('height', '100%');
             $('#carousel-inner-main').css('height', '100%');
             $('.carousel-item').css('height', '100%');
 
-            $('#myCarousel').height(function (index, height) {
-                return (windowHeight - headerHeight);
-            });
-
-            $('#carousel-inner-main').height(function (index, height) {
-                return (windowHeight - headerHeight);
-            });
-
             $('.carousel-item').height(function (index, height) {
                 return (windowHeight - headerHeight);
             });
+
+            $('#myCarousel').height(function (index, height) {
+                return ($('.carousel-item').height());
+            });
+
+            $('#carousel-inner-main').height(function (index, height) {
+                return ($('.carousel-item').height());
+            });
+
         } else {
             $('#myCarousel').css('height', '');
             $('#carousel-inner-main').css('height', '');
