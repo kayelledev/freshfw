3658ff401e9afb77b427b6a31e6987655aafad80
v7m <deathside7@gmail.com>
2015-10-23 15:29:58 +0300

    Assets precompile

---
+++ public/assets/products/products-31f632146966dcf4542a17135b9d7b9fdaa5ab9bc0a57aaf77d5f940cc26bd74.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    // change variant
+    $('input[name=varsku]').on('change', function(e) {
+        var product = $('#product_' + e.target.value);
+        var descr_prod = $('#description_product_' + e.target.value);
+        $('.variant').hide();
+        $(product).show();
+        $(descr_prod).show();
+        $('#buy').attr('href', product.data('url'));
+    });
+
+    // cut description text
+    function resizeProductDescription() {
+        $(".product-desc").each(function(){
+            $(this).dotdotdot({
+                after: "a.readmore",
+                height: 120,
+                callback: function( isTruncated, orgContent ) {
+                    if (isTruncated) {
+                        $(this).find('a.readmore').show();
+                    } else {
+                        $(this).find('a.readmore').hide();
+                    }
+                }
+            });
+        });
+    }
+
+    resizeProductDescription();
+
+    $( window ).resize(function() {
+        resizeProductDescription();
+    });
+});
