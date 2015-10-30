0d6fa2b0fa943c10000e2e9359982cb037779a2f
ZavyalovAndrey <zavyalov79@ukr.net>
2015-10-30 02:46:38 +0200

    social login on staging

--- config/application.rb
+++ config/application.rb
@@ -48,8 +48,6 @@ module FourWalls
             ENV['S3_Secret_Key'] = cfg['S3_Secret_Key'] if ENV['S3_Secret_Key'].nil?
             ENV['S3_Bucket'] = cfg['S3_Bucket'] if ENV['S3_Bucket'].nil?
         end
-    end
-    if Rails.env.production?
         if ENV['TWITTER_API_KEY'].nil? || ENV['TWITTER_API_SECRET'].nil? || ENV['FACEBOOK_APP_ID'].nil? || ENV['FACEBOOK_APP_SECRET'].nil?
             cfg = YAML.load_file("#{Rails.root}/config/social.yml")
             ENV['TWITTER_API_KEY'] = cfg['TWITTER_API_KEY'] if ENV['TWITTER_API_KEY'].nil?
