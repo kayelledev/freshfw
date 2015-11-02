73a744b41992bb27b371abbe873addeccbeba3e8
ZavyalovAndrey <zavyalov79@ukr.net>
2015-11-02 16:29:52 +0200

    add error message to import products

--- shoppe/app/models/shoppe/product.rb
+++ shoppe/app/models/shoppe/product.rb
@@ -166,33 +166,35 @@ module Shoppe
       spreadsheet = open_spreadsheet(file)
       spreadsheet.default_sheet = spreadsheet.sheets.first
       header = spreadsheet.row(1)
-      (2..spreadsheet.last_row).each do |i|
+      errors = []
+      success_count = 0
+      (2..spreadsheet.last_row).each_with_index do |i, index|
         begin
           row = Hash[[header, spreadsheet.row(i)].transpose]
           product = Shoppe::Product.where(name: row["Product Name"], sku: row['SKU']).first_or_create
           product.product_category_id = Shoppe::ProductCategory.where(name: row["Category Name"]).first_or_create.id
           product.product_subcategory_id = Shoppe::ProductCategory.where(name: row["Subcategory Name"]).first_or_create.id
-          product.permalink = row["Permalink"]
-          product.description = row["Description"]
-          product.short_description = row["Short Description"]
+          product.permalink = row["Permalink"] if row["Permalink"]
+          product.description = row["Description"] if row["Description"]
+          product.short_description = row["Short Description"] if row["Short Description"]
           product.save!
-          product.featured = row["Featured"].to_i
-          product.in_the_box = row["What's in the box?"]
-          product.width = row["Width"].to_f
-          product.height = row["Height"].to_f
-          product.depth = row["Depth"].to_f
-          product.seat_width = row["Seat Width"].to_f
-          product.seat_depth = row["Seat Depth"].to_f
-          product.seat_height = row["Seat Height"].to_f
-          product.arm_height = row["Arm Height"].to_f
-          product.price = row["CAD Price"].to_d
-          product.cost_price = row["USA Price"].to_d
-          product.url_default_image = row["Default Image"]
-          product.url_image2 = row["Image2"]
-          product.url_image3 = row["Image3"]
-          product.url_image4 = row["Image4"]
-          product.url_image5 = row["Image5"]
-          product.url_image6 = row["Image6"]
+          product.featured = row["Featured"].to_i if row["Featured"]
+          product.in_the_box = row["What's in the box?"] if row["What's in the box?"]
+          product.width = row["Width"].to_f if row["Width"]
+          product.height = row["Height"].to_f if row["Height"]
+          product.depth = row["Depth"].to_f if row["Depth"]
+          product.seat_width = row["Seat Width"].to_f if row["Seat Width"]
+          product.seat_depth = row["Seat Depth"].to_f if row["Seat Depth"]
+          product.seat_height = row["Seat Height"].to_f if row["Seat Height"]
+          product.arm_height = row["Arm Height"].to_f if row["Arm Height"]
+          product.price = row["CAD Price"].to_d if row["CAD Price"]
+          product.cost_price = row["USA Price"].to_d if row["USA Price"]
+          product.url_default_image = row["Default Image"] if row["Default Image"]
+          product.url_image2 = row["Image2"] if row["Image2"]
+          product.url_image3 = row["Image3"] if row["Image3"]
+          product.url_image4 = row["Image4"] if row["Image4"]
+          product.url_image5 = row["Image5"] if row["Image5"]
+          product.url_image6 = row["Image6"] if row["Image6"]
           product.save!
           field_array.each{|element| row.delete(element)}
           row.each do |key, value|
@@ -202,9 +204,12 @@ module Shoppe
             product_attr.value = value
             product_attr.save!
           end
-        rescue
+        success_count += 1
+        rescue => error
+          errors << {error: error.message, row: index}
         end
       end
+      errors.empty? ? 'Import success' : "Success row count: #{success_count}. Column #{errors.first[:row]}, error: #{errors.first[:error]}"
     end
 
     def self.open_spreadsheet(file)
