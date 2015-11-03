47b006742d2660b084e3c48403974758862a3ab8
ZavyalovAndrey <zavyalov79@ukr.net>
2015-11-03 23:56:12 +0200

    fix smtp

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,7 +66,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
         :port                 => 587,
-        :domain               => "gmail.com",
         :user_name            => "fresh.fourwalls@gmail.com",
         :password             => "Repaforfw",
         :authentication       => :plain,
