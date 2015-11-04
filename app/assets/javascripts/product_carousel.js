d61e2d19a31f53f3b9b8cbb150abc3281adfee74
v7m <deathside7@gmail.com>
2015-11-04 18:26:14 +0200

    Fix images position at product carousel

--- app/assets/javascripts/product_carousel.js
+++ app/assets/javascripts/product_carousel.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
-    var productCarouselWidth = $('#ProductCarousel').width();
-    //resize carousel
-    function resizeCarousel (productCarouselWidth) {
-
-        $('#carousel-inner-product').height(function (index, height) {
-            return ((productCarouselWidth * 2) / 3);
-        });
-
-        $('.carousel-item-product').height(function (index, height) {
-            return ((productCarouselWidth * 2) / 3);
-        });
+  var images = $('#ProductCarousel img.img-responsive');
+  images.each(function() {
+    if ( $(this).height() < $(this).parent().height() ) {
+      $(this).css({
+        'position': 'absolute',
+        'bottom': '0'
+      });
     }
+  });
 
-    resizeCarousel(productCarouselWidth);
+  var productCarouselWidth = $('#ProductCarousel').width();
 
-    $( window ).resize(function() {
-        var productCarouselWidth = $('#ProductCarousel').width();
-        resizeCarousel (productCarouselWidth);
+  resizeCarousel(productCarouselWidth);
+
+  $( window ).resize(function() {
+    var productCarouselWidth = $('#ProductCarousel').width();
+    resizeCarousel (productCarouselWidth);
+  });
+
+  function resizeCarousel (productCarouselWidth) {
+
+    $('#carousel-inner-product').height(function (index, height) {
+      return ((productCarouselWidth * 2) / 3);
     });
+
+    $('.carousel-item-product').height(function (index, height) {
+      return ((productCarouselWidth * 2) / 3);
+    });
+  }
 });
