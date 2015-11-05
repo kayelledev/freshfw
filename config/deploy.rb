95c3ffe271cb1f9d2a7334c98c94cef973d7c735
ZavyalovAndrey <zavyalov79@ukr.net>
2015-11-05 12:34:36 +0200

    configure sidekiq deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rvm_ruby_version, '2.2.2@freshfw'
 set :linked_files, %w{config/database.yml config/secrets.yml config/fog_credentials.yml config/social.yml}
 set :linked_dirs, %w{log tmp/pids public/uploads public/assets public/system public/certificate}
 
-set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
+set :sidekiq_config, -> { File.join(current_path, 'config', 'sidekiq.yml') }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
