7e114f454054d412dbea5661f0fb003adb3f77b6
v7m <deathside7@gmail.com>
2015-11-16 11:40:41 +0200

    Fix product carousel

--- app/assets/javascripts/product_carousel.js
+++ app/assets/javascripts/product_carousel.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  var images = $('#ProductCarousel img.img-responsive');
-  images.each(function() {
-    if ( $(this).height() < $(this).parent().height() ) {
-      $(this).css({
-        'position': 'absolute',
-        'bottom': '0'
-      });
-    }
-  });
 
   var productCarouselWidth = $('#ProductCarousel').width();
 
