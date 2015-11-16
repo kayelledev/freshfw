13ce1cdc82f04a9fd6d9c015015ac230178af7e2
v7m <deathside7@gmail.com>
2015-11-16 13:31:20 +0200

    Fix product carousel

--- app/assets/javascripts/product_carousel.js
+++ app/assets/javascripts/product_carousel.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
   var productCarouselWidth = $('#ProductCarousel').width();
 
   resizeCarousel(productCarouselWidth);
+  fitImages();
 
   $( window ).resize(function() {
     var productCarouselWidth = $('#ProductCarousel').width();
@@ -17,6 +17,28 @@ $(document).ready(function() {
 
     $('.carousel-item-product').height(function (index, height) {
       return ((productCarouselWidth * 2) / 3);
+    });
+  }
+
+  function fitImages() {
+    var images = $('#ProductCarousel .carousel-inner .carousel-item-product img');
+    $(images).each(function() {
+      $(this).load(function() {
+        if ( $(this)[0].naturalHeight >= $(this)[0].naturalWidth ) {
+          $(this).css({
+            'height': '100%',
+            'width': 'auto'
+          });
+        } else {
+          $(this).css({
+            'position': 'absolute',
+            'bottom': '0',
+            'max-height': $('#ProductCarousel .carousel-inner')
+          });
+        }
+      }).each(function() {
+        if(this.complete) $(this).load();
+      });
     });
   }
 });
