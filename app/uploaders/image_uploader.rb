3b3e1671d23f90d9f8ce38b66237403478a22ad1
Viacheslav <limoozyn@gmail.com>
2015-11-23 15:43:51 +0200

    Fix save to file furniture plan with CORS workaround

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   def cache_dir
       '/tmp/fw-cache'
-    end
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
