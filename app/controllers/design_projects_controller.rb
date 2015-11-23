1354b93df462fda1a0932d3b4e9de51c2aa1ae6e
v7m <deathside7@gmail.com>
2015-11-23 18:00:13 +0200

    Add create action ande route

--- app/controllers/design_projects_controller.rb
+++ app/controllers/design_projects_controller.rb
@@ -1,6 +1,11 @@
 class DesignProjectsController < ApplicationController
  load_and_authorize_resource :class => 'DesignProjectsController'
- 
+
+  def create
+    @design_project = DesignProject.new(design_project_params)
+    @design_project.save
+  end
+
   def project_info
   end
 
@@ -12,5 +17,11 @@ class DesignProjectsController < ApplicationController
   end
 
   def instructions
+  end
+
+  private
+
+  def design_project_params
+    params[:design_project].permit(:name, :inspiration, :inspiration_image1, :inspiration_image2, :inspiration_image3, :user_id, :room_type_id, :width, :depth)
   end
 end
