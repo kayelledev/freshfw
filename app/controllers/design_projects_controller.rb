a788460bb5c87439dba21c65f3f2e026e906cc9c
Tania Filonenko <Tania.Filonenko@gmail.com>
2015-11-25 23:03:46 +0200

    add saving for exist or new or saved in session

--- app/controllers/design_projects_controller.rb
+++ app/controllers/design_projects_controller.rb
@@ -3,10 +3,9 @@ class DesignProjectsController < ApplicationController
     before_filter { (params[:id] && @design_project = DesignProject.find(params[:id])) ||
                     (session[:design_project_id] && @design_project = DesignProject.find(session[:design_project_id]) )
                   }
-    # before_filter check_session_contain_pro
+    before_filter :check_session_contain_project, only: [:create]
 
   def create
-    binding.pry
     @design_project = DesignProject.new(design_project_params)
     @design_project.user = current_user
     @design_project.status = :draft
@@ -24,7 +23,26 @@ class DesignProjectsController < ApplicationController
     end
   end
 
+  def update
+    if @result = @design_project.update(design_project_params)
+      respond_to do |format|
+        format.html { redirect_to designer_portal_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render action: :project_info }
+        format.js
+      end
+    end
+  end
+
+  def check_session_contain_project
+    session[:design_project_id] ? update : create 
+  end
+
   def project_info
+    session[:design_project_id] = params[:id] if params[:id].present?
     @design_project = session[:design_project_id] ? DesignProject.find(session[:design_project_id]) : Shoppe::DesignProject.new
   end
 
@@ -44,8 +62,7 @@ class DesignProjectsController < ApplicationController
 
   def add_to_room_builder
     @design_project.update(product_ids: params[:product_ids])
-    select_items
-    render 'select_items'
+    redirect_to designer_portal_select_items_path
   end
 
   def remove_product
@@ -66,25 +83,26 @@ class DesignProjectsController < ApplicationController
                                   .joins(:product_category).order('shoppe_product_categories.name')
                                   .group_by { |t| t.product_category.name }
 
-    filters_array = []
-    params[:categories].try(:each) do |category_id|
-      category = ProductCategory.find(category_id)
-      filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::ProductCategory').first_or_create
-      filters_array << filter
+    unless params[:categories].nil? && params[:colors].nil? && params[:materials].nil? 
+      filters_array = []
+      params[:categories].try(:each) do |category_id|
+        category = ProductCategory.find(category_id)
+        filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::ProductCategory').first_or_create
+        filters_array << filter
+      end
+      params[:colors].try(:each) do |category_id|
+        category = Color.find(category_id)
+        filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::Color').first_or_create
+        filters_array << filter
+      end
+      params[:materials].try(:each) do |category_id|
+        category = ProductCategory.find(category_id)
+        filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::Material').first_or_create
+        filters_array << filter
+      end
+      filters = Filter.where(id: filters_array.map(&:id))
+      @design_project.update(filter_ids: filters.ids)
     end
-    params[:colors].try(:each) do |category_id|
-      category = Color.find(category_id)
-      filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::Color').first_or_create
-      filters_array << filter
-    end
-    params[:materials].try(:each) do |category_id|
-      category = ProductCategory.find(category_id)
-      filter = Filter.where(filter_element_id: category_id, filter_element_type: 'Shoppe::Material').first_or_create
-      filters_array << filter
-    end
-    filters = Filter.where(id: filters_array.map(&:id))
-    @design_project.update(filter_ids: filters.ids)
-
     respond_to do |format|
       format.js
     end
@@ -103,6 +121,6 @@ class DesignProjectsController < ApplicationController
   def design_project_params
     width = ( params[:width_ft].to_i * 12 ) + params[:width_in].to_i
     depth = ( params[:depth_ft].to_i * 12 ) + params[:depth_in].to_i
-    params.require(:design_project).permit(:name, :inspiration, :inspiration_image1, :inspiration_image2, :inspiration_image3, :room_type_id, :url_inspiration_image1, :url_inspiration_image2, :url_inspiration_image3, :inspiration_image1, :inspiration_image2, :inspiration_image3).merge(width: width, depth: depth)
+    params.require(:design_project).permit(:name, :inspiration, :inspiration_image1, :inspiration_image2, :inspiration_image3, :product_category_id, :url_inspiration_image1, :url_inspiration_image2, :url_inspiration_image3, :inspiration_image1, :inspiration_image2, :inspiration_image3).merge(width: width, depth: depth)
   end
 end
