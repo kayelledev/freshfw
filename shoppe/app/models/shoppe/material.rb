130888e697c85630edf8ed6cc104b1d65d47c0c7
Tania Filonenko <Tania.Filonenko@gmail.com>
2015-12-02 21:51:17 +0200

    depend destroy for color category and materials

--- shoppe/app/models/shoppe/material.rb
+++ shoppe/app/models/shoppe/material.rb
@@ -3,6 +3,6 @@ module Shoppe
   	validates :name, :presence => true
   	# has_many  :products, :class_name => 'Shoppe::Product'
   	has_and_belongs_to_many :products, :join_table => :shoppe_products_materials
-  	has_many :filters, as: :filter_element, :class_name => 'Shoppe::Filter'
+  	has_many :filters, as: :filter_element, :class_name => 'Shoppe::Filter', :dependent => :destroy
   end
 end
